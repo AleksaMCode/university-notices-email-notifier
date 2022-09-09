6a9a1172b1e92656003a24591d40bcb2409bf65d
AleksaMCode <aleksamcode@gmail.com>
2022-09-09 22:20:12 +0200

    Method get gets 'oglasi' for every year

--- data_extractor.py
+++ data_extractor.py
@@ -13,32 +13,33 @@ class DataExtractor:
         self.web_driver = web_driver
         self.latest_read_time = datetime.strptime(latest_read_time, '%d/%m/%y %H:%M:%S')
         self.soup = BeautifulSoup(self.web_driver.page_source, "html.parser")
-        ulist = self.soup.find('ul', id='ul_id_1')
-        self.oglasi = ulist.find_all('li')
 
     def get_oglasi(self):
         oglasi_list = []
         oglas_info = []
-        for oglas in self.oglasi:
-            h2_headings = oglas.find_all('h2')
-            for i in range(2):
-                if h2_headings[i] is not "":
-                    dt = datetime.strptime(h2_headings[i].text, '%d/%m/%y %H:%M:%S')
-                    if self.latest_read_time > dt:
-                        continue
-                oglas_info.insert(h2_headings[i].text)
+        for year in range(1, 5):
+            ulist = self.soup.find('ul', id=f'ul_id_{i}')
 
-            oglas_info.insert(oglas.h1.text)
-            oglas_info.insert(oglas.p.text)
-            if oglas.a is not None:
-                attachment = oglas.find('a', href=True)
-                oglas_info.insert(attachment.contents[0])
-                oglas_info.insert(attachment['href'])
-            else:
-                oglas_info.insert("")
-                oglas_info.insert("")
-        oglasi_list.insert(
-            Oglas(ime_predmeta=oglas_info[2], date=oglas_info[0], title=oglas_info[1], content=oglas_info[3],
-                  attachment_text=oglas_info[4], attachment_link=oglas_info[5]))
+            for oglas in ulist.find_all('li'):
+                h2_headings = oglas.find_all('h2')
+                for i in range(2):
+                    if h2_headings[i] is not "":
+                        dt = datetime.strptime(h2_headings[i].text, '%d/%m/%y %H:%M:%S')
+                        if self.latest_read_time > dt:
+                            continue
+                    oglas_info.insert(h2_headings[i].text)
+
+                oglas_info.insert(oglas.h1.text)
+                oglas_info.insert(oglas.p.text)
+                if oglas.a is not None:
+                    attachment = oglas.find('a', href=True)
+                    oglas_info.insert(attachment.contents[0])
+                    oglas_info.insert(attachment['href'])
+                else:
+                    oglas_info.insert("")
+                    oglas_info.insert("")
+            oglasi_list.insert(
+                Oglas(ime_predmeta=oglas_info[2], year=year, date=oglas_info[0], title=oglas_info[1],
+                      content=oglas_info[3], attachment_text=oglas_info[4], attachment_link=oglas_info[5]))
 
         return oglasi_list
