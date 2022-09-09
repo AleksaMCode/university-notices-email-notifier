57253aabf35ef2ad9661286643d149f1a81a9a3d
AleksaMCode <aleksamcode@gmail.com>
2022-09-09 23:09:03 +0200

    Config file parsing implemented

---
+++ utilities/settings_parser.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+from settings import Settings
+
+
+class SettingsParse:
+    SETTINGS_CONFIG: str = "etfbl-oglasi\settings.json"
+
+    @staticmethod
+    def save(settings: Settings):
+        path = os.getenv('APPDATA')
+        os.mkdir(path)
+
+        json_object = vars(settings)
+        with open(SettingsParse.SETTINGS_CONFIG, "w") as outfile:
+            outfile.write(json_object)
+
+    @staticmethod
+    def read() -> Settings:
+        if not os.path.exists(SettingsParse.SETTINGS_CONFIG):
+            SettingsParse.save(Settings())
+        else:
+            with open(SettingsParse.SETTINGS_CONFIG) as infile:
+                data = json.load(infile)
+                return data
