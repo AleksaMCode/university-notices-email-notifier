a75b0f40d5bb1144dd8ac56cfda4dd5e6cc2e321
AleksaMCode <aleksamcode@gmail.com>
2022-11-30 14:44:48 +0100

    Data extractor updated to include Notice and Notice Builder

--- web_handler/data_extractor.py
+++ web_handler/data_extractor.py
@@ -1,4 +1,4 @@
-from database_manager.oglas import Oglas
+from notice import NoticeBuilder
 from web_driver import WebDriver
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -7,40 +7,47 @@ from datetime import datetime
 class DataExtractor:
     web_driver: WebDriver
     soup: BeautifulSoup
-    last_fetch: datetime
+    latest_fetch: datetime
     DATETIME_FORMAT: str = "%d/%m/%y %H:%M:%S"
 
-    def __init__(self, web_driver, last_fetch: str):
+    def __init__(self, web_driver, latest_fetch: str):
         self.web_driver = web_driver
-        self.last_fetch = datetime.strptime(last_fetch, self.DATETIME_FORMAT)
+        self.latest_fetch = latest_fetch
         self.soup = BeautifulSoup(self.web_driver.page_source, "html.parser")
 
-    def get_oglasi(self):
-        oglasi_list = []
-        oglas_info = []
+    def _set_latest_fetch(self, latest_fetch):
+        self.latest_fetch = datetime.today().strptime(self.DATETIME_FORMAT)
+
+    def get_notice(self):
+        notices = []
+        notice_builder = NoticeBuilder()
         for year in range(1, 5):
-            ulist = self.soup.find('ul', id=f'ul_id_{i}')
+            ulist = self.soup.find('ul', id=f'ul_id_{year}')
 
-            for oglas in ulist.find_all('li'):
-                h2_headings = oglas.find_all('h2')
+            for notice in ulist.find_all('li'):
+                h2_headings = notice.find_all('h2')
                 for i in range(2):
-                    if h2_headings[i] is not "":
+                    # if notice doesn't have datetime set or
+                    # latest fetch datetime isn't set (program is run for the fist time), ignore this check
+                    if not h2_headings[i] or not self.latest_fetch:
                         dt = datetime.strptime(h2_headings[i].text, self.DATETIME_FORMAT)
-                        if self.latest_read_time > dt:
+                        if self.latest_fetch > dt:
                             continue
-                    oglas_info.insert(h2_headings[i].text)
 
-                oglas_info.insert(oglas.h1.text)
-                oglas_info.insert(oglas.p.text)
-                if oglas.a is not None:
-                    attachment = oglas.find('a', href=True)
-                    oglas_info.insert(attachment.contents[0])
-                    oglas_info.insert(attachment['href'])
-                else:
-                    oglas_info.insert("")
-                    oglas_info.insert("")
-            oglasi_list.insert(
-                Oglas(subject=oglas_info[2], year=year, date=oglas_info[0], title=oglas_info[1],
-                      content=oglas_info[3], attachment_text=oglas_info[4], attachment_link=oglas_info[5]))
+                    if i == 0:
+                        notice_builder.set_date(h2_headings[i].text)
+                    else:  # i == 1
+                        notice_builder.set_text(h2_headings[i].text)
 
-        return oglasi_list
+                notice_builder.set_subject(notice.h1.text)
+                notice_builder.set_content(notice.p.text)
+
+                if notice.a is not None:
+                    attachment = notice.find('a', href=True)
+                    notice_builder.set_text(attachment.contents[0])
+                    notice_builder.set_link(attachment['href'])
+
+            notices.insert(notice_builder.notice())
+
+        self._set_latest_fetch()
+        return notices
