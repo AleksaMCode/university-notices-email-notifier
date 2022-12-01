6f179ce00946fcf340df1c6ca7eb517e9928cf53
AleksaMCode <aleksamcode@gmail.com>
2022-12-01 19:18:06 +0100

    Email handler added

---
+++ email_handler/email_handler.py
@@ -0,0 +1,31 @@
+import configparser
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+def send_email():
+    server = smtplib.SMTP(config['SMTP']['smtp'], config['SMTP']['port'])
+    server.starttls()
+    server.login(config['SMTP']['email'], config['SMTP']['password'])
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Notices ETFBL"
+    message["From"] = config['SMTP']['email']
+    message["To"] = config['SMTP']['user_email']
+    part = MIMEText("You can find ETFBL notices attached in json file.", "plain")
+    message.attach(part)
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open("notices.json", "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename=notices-etfbl.json')
+    message.attach(part)
+
+    server.sendmail(from_addr=config['SMTP']['email'], to_addrs=config['SMTP']['user_email'], msg=message.as_string())
+    server.quit()
