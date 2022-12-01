0baabe22e34336380a005af6f12d5994ff6c239c
AleksaMCode <aleksamcode@gmail.com>
2022-12-01 16:28:27 +0100

    Settings handler removed

--- settings_handler/settings.py
+++
@@ -1,7 +0,0 @@
-from typing_extensions import TypedDict
-
-
-class Settings(TypedDict):
-    website: str = "https://efee.etf.unibl.org/oglasi/"
-    latest_fetch: str = ""
-    web_driver: str = "CHROME"
