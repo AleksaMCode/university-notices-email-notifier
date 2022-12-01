3d5cc18c16c227e2ccbc2b83c2053105355aa51a
AleksaMCode <aleksamcode@gmail.com>
2022-12-01 21:45:11 +0100

    Timestamp bugs removed

--- web_handler/web_scraper.py
+++ web_handler/web_scraper.py
@@ -20,31 +20,32 @@ class WebScraper:
         self.soup = BeautifulSoup(self.web_driver.page_source, "html.parser")
         atexit.register(self.cleanup)
 
-    def _set_latest_fetch(self, latest_fetch: str = ""):
-        self.latest_fetch = datetime.today().strftime(self.DATETIME_FORMAT) if not latest_fetch else None
+    def _set_latest_fetch(self, latest_fetch):
+        config['SCRAPER']['latest_fetch'] = latest_fetch
 
     def scrape(self):
         notices = []
         notice_builder = NoticeBuilder()
+        timestamp = datetime.today().strftime(self.DATETIME_FORMAT)
         for year in range(1, 5):
             ulist = self.soup.find('ul', id=f'ul_id_{year}')
 
             for notice in ulist.find_all('li'):
                 h2_headings = notice.find_all('h2')
-                for i in range(2):
-                    # if notice doesn't have datetime set or
-                    # latest fetch datetime isn't set (program is run for the fist time), ignore this check
-                    if not self.latest_fetch and not h2_headings[i]:
-                        dt = datetime.strptime(h2_headings[i].text, self.DATETIME_FORMAT)
-                        if self.latest_fetch > dt:
-                            continue
 
-                    notice_builder.set_year(year)
+                # if notice doesn't have datetime set or
+                # latest fetch datetime isn't set (program is run for the fist time), ignore this check
+                str_timestamp = h2_headings[0].text
+                if config['SCRAPER']['latest_fetch'] and str_timestamp:
+                    notice_timestamp = datetime.strptime(str_timestamp, self.DATETIME_FORMAT)
+                    current_timestamp = datetime.strptime(config['SCRAPER']['latest_fetch'], self.DATETIME_FORMAT)
+                    if current_timestamp > notice_timestamp:
+                        continue
 
-                    if i == 0:
-                        notice_builder.set_date(h2_headings[i].text)
-                    else:  # i == 1
-                        notice_builder.set_text(h2_headings[i].text)
+                notice_builder.set_year(year)
+
+                notice_builder.set_date(str_timestamp)
+                notice_builder.set_text(h2_headings[1].text)
 
                 notice_builder.set_subject(notice.h1.text)
                 notice_builder.set_content(notice.p.text)
@@ -56,9 +57,10 @@ class WebScraper:
 
                 notices.append(notice_builder.notice.__dict__)
 
-        self._set_latest_fetch()
+        self._set_latest_fetch(timestamp)
 
-        self._write_notices_to_json(notices)
+        if notices:
+            self._write_notices_to_json(notices)
 
     def _write_notices_to_json(self, notices):
         with open(config['SCRAPER']['notices'], 'w', encoding='utf-8') as json_file:
