ae7efac59432b48f3b8174ea139c01b5357e9616
AleksaMCode <aleksamcode@gmail.com>
2022-12-02 11:53:21 +0100

    Typo removed

--- resources/scripts/scraper_creator.ps1
+++ resources/scripts/scraper_creator.ps1
@@ -1,3 +1,2 @@
 New-Item scraper.bat
-"@echo of `r`n""C:\Users\Username\AppData\Local\Programs\Python\Python310\python.exe"" ""C:\Users\Username\university-notices-email-notifier\notifier.py""
-```
+"@echo of `r`n""C:\Users\Username\AppData\Local\Programs\Python\Python310\python.exe"" ""C:\Users\Username\university-notices-email-notifier\notifier.py"""
